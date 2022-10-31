9a4151b491fd34eda887771d4f6460ecef1869ef
Shafeeq <shafeeqsha1510@gmail.com>
2022-10-31 08:54:01 -0700

    Header logo and search box added

--- Apps.js
+++ Apps.js
@@ -1,9 +1,26 @@
 import react from "react";
+import "./App.css"
+import Headers from "./Headers";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 export default function Apps(){
     return(
-        <div>
-            <h1>Hello</h1>
+      <Router>
+          <div className="app">
+              <Switch>
+                  <Route path="/checkout">
+                      <h1>Checkout</h1>
 
-        </div>
+                  </Route>
+                  <Route path="/login">
+                      <h1>Login page</h1>
+
+                  </Route>
+                  <Route path="/">
+                      <Headers />
+                      <h1>Home page</h1>
+                  </Route>
+              </Switch>
+          </div>
+      </Router>
     )
 }
