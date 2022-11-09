c29c9037dcc3ebff1bf46fdce08e4852beecc229
Shafeeq <shafeeqsha1510@gmail.com>
2022-11-09 07:28:13 -0800

    StateProvider added

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 
 import Apps from './Apps';
 import { BrowserRouter as Router} from 'react-router-dom';
+import { StateProvider } from './StateProvider';
+import reducer, {initialState} from "./reducer"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
+   <StateProvider initialState={initialState} reducer={reducer}>
    <Apps/>
+
+   </StateProvider>
+  
  </React.StrictMode>
 );
 
